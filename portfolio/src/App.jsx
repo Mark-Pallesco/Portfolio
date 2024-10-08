b4755b6f0bd83e024e59c15b1fd80c09bd5374cc
Mark-Pallesco <angelomark31@gmail.com>
2024-10-08 22:45:52 +0800

    header

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./Components/Header"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+    <Header/>
+    </div>
+  
   )
 }
