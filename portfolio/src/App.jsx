bb310d8dd5a91fe90bd4b7433609f2d9511af0ca
Mark-Pallesco <angelomark31@gmail.com>
2024-10-09 20:59:54 +0800

    added threejs

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,18 +1,21 @@
-import Header from "./Components/Header"
+import Header from "./Components/Header";
 
 export default function App() {
-  return (
-    <div className="bg-darkbg" >
-    <Header/>
-      <div className="w-full h-screen flex justify-center items-center" id="home">
-        <div className="text-white" > 
-           <h3 className="font-sans text-3xl" >Hi there, I am</h3>
-           <h1>Mark Angelo Pallesco</h1>
-           <h2>Im a Front-End Developer</h2>
-           <button className="px-8 py-2 bg-transparent text-xl text-white rounded-3xl border border-white" >Learn More</button>
-        </div>
-      </div>
-    </div>
+
   
-  )
+  return (
+    <div className="bg-darkbg">
+      <Header />
+      <main className="w-full h-screen flex justify-center items-center" id="info">
+        <section className="text-white gap-4 flex flex-col items-center">
+          <h3 className="font-sans text-3xl">Hi there, I am</h3>
+          <h1 className="font-bold text-5xl">Mark Angelo Pallesco</h1>
+          <h2 className="text-2xl">Im a Front-End Developer</h2>
+          <button className="px-8 py-2 bg-transparent text-xl text-white rounded-3xl border border-white hover:bg-white hover:text-darkbg transition duration-300">
+            Learn More
+          </button>
+        </section>
+      </main>
+    </div>
+  );
 }
