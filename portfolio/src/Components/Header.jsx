12a672495e24335bd398a2280a1262608de30573
Mark-Pallesco <angelomark31@gmail.com>
2024-10-11 16:56:19 +0800

    back

--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <div className="absolute w-full text-white" >
             <div  className="px-10 py-5 relative">
                 <header className="flex justify-between" >
-                    <h3 className="text-4xl text-white font-bold" >mark.dev</h3>
+                    <h3 className="text-4xl text-white font-bold" >lorem.dev</h3>
                     <nav>
                         <ul className="flex gap-4" >
                         <li className="hover:bg-white hover:text-darkbg p-2 rounded-sm "><a href="#home">Home</a></li>
