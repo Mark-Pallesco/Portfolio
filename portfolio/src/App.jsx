d0fd9480a48a529994fe3623c04a475f1e740a32
Mark-Pallesco <angelomark31@gmail.com>
2024-10-17 11:41:27 +0800

    bakit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Components/Header";
 import { useState } from 'react';
 
 export default function App() {
-  const [name, setName] = useState('Marvin Tamayo');
+  const [name, setName] = useState('Lorem Lorem');
   const [inputValue, setInputValue] = useState(''); 
 
   const handleChange = (event) => {
@@ -22,21 +22,23 @@ export default function App() {
           <h3 className="font-sans text-2xl sm:text-xl">Lorem, ipsum.</h3>
           <h1 className="font-bold text-5xl md:text-4xl sm:text-3xl">{name}</h1>
           <h2 className="text-2xl sm:text-xl">Lorem, ipsum dolor.</h2>
-          <input 
-            type="text" 
-            value={inputValue} 
-            onChange={handleChange} 
-            placeholder="Enter a new name"
-            className="px-4 py-2 rounded text-darkbg"
-          />
           <button 
-            onClick={updateName} 
             className="px-8 py-2 bg-transparent text-xl text-white rounded-3xl border border-white hover:bg-white hover:text-darkbg transition duration-300"
           >
-            Update Name
+            Learn More
           </button>
         </section>
       </main>
+
+      <div id="about" >
+          <div className="w-full" >
+                <div className="text-white" >
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi autem obcaecati corporis minus, facilis perferendis suscipit quibusdam sit? Autem, repudiandae?</p>
+                    <h2>Lorem Lorem</h2>
+                </div>
+          </div>
+      </div>
+
     </div>
   );
 }
