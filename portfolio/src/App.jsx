a15eb4db4927ccec1c04e38dd72f9d6469887fb9
Mark-Pallesco <angelomark31@gmail.com>
2024-11-27 15:15:20 +0800

    last

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -17,8 +17,8 @@ export default function App() {
   return (
     <div className="bg-darkbg">
       <Header />
-      <main className="w-full h-screen flex justify-center items-center" id="info">
-        <section className="text-white gap-4 flex flex-col items-center w-full">
+      <main className="w-full h-screen flex justify-center items-center bg-grid-gray-500 bg-grid-8" id="info">
+        <section className="text-white gap-4 flex flex-col items-center w-full z-10 ">
           <h3 className="font-sans text-2xl sm:text-xl">Lorem, ipsum.</h3>
           <h1 className="font-bold text-5xl md:text-4xl sm:text-3xl">{name}</h1>
           <h2 className="text-2xl sm:text-xl">Lorem, ipsum dolor.</h2>
--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-        <div className="absolute w-full text-white" >
+        <div className="absolute w-full text-white z-10 bg-darkbg border-b border-white border-opacity-50" >
             <div  className="px-10 py-5 relative">
                 <header className="flex justify-between" >
                     <h3 className="text-4xl text-white font-bold" ><a href="#">mark.dev</a></h3>
